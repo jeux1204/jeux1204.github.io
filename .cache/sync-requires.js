cf2a5a8716d14b7d162ea5a83034a17cbf85f848
jeux1204 <jeux1204@naver.com>
2020-07-05 06:08:33 -0400

    Added remain files for Gatsby default blog

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,9 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ericalee/Documents/GitHub/jeux1204.github.io/.cache/dev-404-page.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/ericalee/Documents/GitHub/jeux1204.github.io/src/pages/index.js")))
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/ericalee/Documents/GitHub/jeux1204.github.io/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/ericalee/Documents/GitHub/jeux1204.github.io/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/ericalee/Documents/GitHub/jeux1204.github.io/src/pages/page-2.js"))),
+  "component---src-pages-using-typescript-tsx": hot(preferDefault(require("/Users/ericalee/Documents/GitHub/jeux1204.github.io/src/pages/using-typescript.tsx")))
 }
 
