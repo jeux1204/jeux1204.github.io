5e9a513a97355508673896cebf614ddcc6557f21
jeux1204 <jeux1204@naver.com>
2020-07-15 18:15:09 -0400

    Fixed head and added navigation links

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,43 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import profile from "../images/Erica.jpg"
+import ListLink from "./listlink"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header>
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        maxWidth: 1080,
+        minHeight: 200,
+        padding: `2rem 1.0875rem`,
+        position: `relative`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <div className="box" style={{backgroundColor: `#BDBDBD`}}>
+        <img className="profileimg" src={profile} alt="Erica and Cheong-ah"/>
+      </div>
+      <div className="mainheader">
+        <h1 style={{margin: `1rem 0 0 0`}}>
+          <Link
+            to="/"
+            style={{
+              color: `#0f1a77`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <h3 style={{margin: `0`}}>Software engineer</h3>
+      </div>
+      <ul className="navlink">
+        <ListLink to="/">About me</ListLink>
+        <ListLink to="/resume/">Resume</ListLink>
+        <ListLink to="/blog/">Blog</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
     </div>
   </header>
 )
---
+++ src/components/listlink.js
@@ -0,0 +1,11 @@
+
+import React from "react"
+import { Link } from "gatsby"
+
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link style={{ textDecoration: `none` }} to={props.to}>{props.children}</Link>
+  </li>
+)
+
+export default ListLink
